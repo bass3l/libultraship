a028e6aca01dbc683aeb90b94802f3c88a8e9c41
Bassel Alshmali <bassel@mobilabsolutions.com>
2026-04-17 23:14:12 +0200

    Add G_SETTIMG_PAL and G_INVAL_TEX_BY_PAL

--- include/fast/lus_gbi.h
+++ include/fast/lus_gbi.h
@@ -66,11 +66,13 @@ constexpr int8_t OTR_G_DL_INDEX = OPCODE(0x3d);
 constexpr int8_t OTR_G_READFB = OPCODE(0x3e);
 constexpr int8_t OTR_G_REGBLENDEDTEX = OPCODE(0x3f);
 constexpr int8_t OTR_G_SETINTENSITY = OPCODE(0x40);
+constexpr int8_t OTR_G_SETTIMG_PAL = OPCODE(0x41);
 constexpr int8_t OTR_G_MOVEMEM_HASH = OPCODE(0x42);
 constexpr int8_t OTR_G_PUSH_SHADER = OPCODE(0x43);
 constexpr int8_t OTR_G_POP_SHADER = OPCODE(0x44);
 constexpr int8_t RDP_G_SETTILESIZE_INTERP = OPCODE(0x45);
 constexpr int8_t RDP_G_SETTARGETINTERPINDEX = OPCODE(0x46);
+constexpr int8_t OTR_G_INVAL_TEX_BY_PAL = OPCODE(0x47);
 
 /*
  * The following commands are the "generated" RDP commands; the user
